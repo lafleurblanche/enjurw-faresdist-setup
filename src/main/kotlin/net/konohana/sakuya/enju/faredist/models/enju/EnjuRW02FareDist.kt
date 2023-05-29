e6eded308907b79eb8d3048ae52656bfccdae5ae
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-29 23:41:27 +0900

    20230529 ADD EnjuRW02FareDist

---
+++ src/main/kotlin/net/konohana/sakuya/enju/faredist/models/enju/EnjuRW02FareDist.kt
@@ -0,0 +1,16 @@
+package net.konohana.sakuya.enju.faredist.models.enju
+
+import org.jetbrains.exposed.sql.Table
+
+object EnjuRW02FareDist : Table("enju_rw02_faredist") {
+    /** ID(自動採番) */
+    val id = integer("id").autoIncrement()
+    /** 駅名コード */
+    val staCode = varchar("sta_code", length = 20)
+    /** 駅名 */
+    val staName = varchar("sta_name", length = 20)
+    /** 起点駅コード */
+    val startingPointStaCode = varchar("strt_pt_sta_code", length = 20)
+    /** 営業キロ */
+    val distance = double("distance")
+}
