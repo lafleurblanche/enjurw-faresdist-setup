68e4dbfb1baaabbb1447fd6fa4232a52205bcd5e
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-30 10:43:49 +0900

    20230529 ADD Route

--- src/main/kotlin/net/konohana/sakuya/enju/faredist/controller/EnjuRWFareDistCrudController.kt
+++ src/main/kotlin/net/konohana/sakuya/enju/faredist/controller/EnjuRWFareDistCrudController.kt
@@ -7,6 +7,11 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import net.konohana.sakuya.enju.faredist.models.enju.EnjuRW01FareDist
 import net.konohana.sakuya.enju.faredist.models.enju.EnjuRW02FareDist
+import net.konohana.sakuya.enju.faredist.models.enju.EnjuRW03FareDist
+import net.konohana.sakuya.enju.faredist.models.enju.EnjuRW04FareDist
+import net.konohana.sakuya.enju.faredist.models.enju.EnjuRW05FareDist
+import net.konohana.sakuya.enju.faredist.models.enju.EnjuRW06FareDist
+import net.konohana.sakuya.enju.faredist.models.enju.EnjuRW07FareDist
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -27,6 +32,46 @@ fun Route.enjuRWFareDistCrudController() {
                         SchemaUtils.create(EnjuRW02FareDist)
                     }
                     call.respond(mapOf("Created:" to "EnjuRW02FareDist"))
+                }
+            }
+            route("enjurw03faredist") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW03FareDist)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW03FareDist"))
+                }
+            }
+            route("enjurw04faredist") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW04FareDist)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW04FareDist"))
+                }
+            }
+            route("enjurw05faredist") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW05FareDist)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW05FareDist"))
+                }
+            }
+            route("enjurw06faredist") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW06FareDist)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW06FareDist"))
+                }
+            }
+            route("enjurw07faredist") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW07FareDist)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW07FareDist"))
                 }
             }
         }
