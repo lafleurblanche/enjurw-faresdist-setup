a0b1b8ef6a8f10170b1043ec4cf40e0659b0b923
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-05 22:45:40 +0900

    20230605 NEW

---
+++ src/main/kotlin/net/konohana/sakuya/enju/faredist/models/enju/EnjuRW16FareDist.kt
@@ -0,0 +1,21 @@
+package net.konohana.sakuya.enju.faredist.models.enju
+
+import org.jetbrains.exposed.sql.Table
+
+/**
+ * ## EnjuRW16FareDistモデル
+ * * 槐谷線営業キロ情報
+ * @author lafleurblanche
+ */
+object EnjuRW16FareDist : Table("enju_rw16_faredist") {
+    /** ID(自動採番) */
+    val id = integer("id").autoIncrement()
+    /** 駅名コード */
+    val staCode = varchar("sta_code", length = 20)
+    /** 駅名 */
+    val staName = varchar("sta_name", length = 20)
+    /** 起点駅コード */
+    val startingPointStaCode = varchar("strt_pt_sta_code", length = 20)
+    /** 営業キロ */
+    val distance = double("distance")
+}
